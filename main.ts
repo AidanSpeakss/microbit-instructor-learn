c56c2cc0e5b1ac25689f101d9c411df0c86bf9be
Aidan Raney <24593916+AidanSpeakss@users.noreply.github.com>
2021-07-23 15:02:08 -0500

    add fun thing for friend, and use showstring

--- main.ts
+++ main.ts
@@ -1,16 +1,127 @@
-basic.forever(function () {
-    basic.showLeds(`
-        . # . # .
-        . # . # .
-        . . . . .
-        # . . . #
-        . # # # .
-        `)
-    basic.showLeds(`
-        . # . # .
-        . # . # .
-        . . . . .
-        . # # # .
-        # . . . #
-        `)
-})
+// basic.forever(function () {
+//     basic.showLeds(`
+//         . # # # .
+//         . # . . .
+//         . # # # .
+//         . . . # .
+//         . # # # .
+//         `)
+//     basic.pause(100)
+//     basic.showLeds(`
+//         . # . # .
+//         . # . # .
+//         . # # # .
+//         . # . # .
+//         . # . # .
+//         `)
+//     basic.pause(100)
+//     basic.showLeds(`
+//         . # . # .
+//         . # . # .
+//         . # . # .
+//         . # . # .
+//         . # # # .
+//         `)
+//     basic.pause(100)
+//     basic.showLeds(`
+//         . # # # .
+//         . . # . .
+//         . . # . .
+//         . . # . .
+//         . . # . .
+//         `)
+//     basic.showLeds(`
+//         . . . . .
+//         . . . . .
+//         . . . . .
+//         . . . . .
+//         . . . . .
+//         `)
+//     basic.pause(100)
+//     basic.showLeds(`
+//         . # . # .
+//         . # . # .
+//         . # . # .
+//         . # . # .
+//         . # # # .
+//         `)
+//     basic.pause(100)
+//     basic.showLeds(`
+//         . # # # .
+//         . # . # .
+//         . # # # .
+//         . # . . .
+//         . # . . .
+//         `)
+//     basic.pause(100)
+//     basic.showLeds(`
+//         . . . . .
+//         . . . . .
+//         . . . . .
+//         . . . . .
+//         . . . . .
+//         `)
+//     basic.pause(100)
+//     basic.showLeds(`
+//         . # . # .
+//         . # . # .
+//         . . # . .
+//         . # . # .
+//         . # . # .
+//         `)
+//     basic.pause(100)
+//     basic.showLeds(`
+//         . . # . .
+//         . # . # .
+//         . # # # .
+//         . # . # .
+//         . # . # .
+//         `)
+//     basic.pause(100)
+//     basic.showLeds(`
+//         . . . . .
+//         . # # . .
+//         . # . # .
+//         . # . # .
+//         . # . # .
+//         `)
+//     basic.pause(100)
+//     basic.showLeds(`
+//         . # # . .
+//         . # . # .
+//         . # . # .
+//         . # . # .
+//         . # # . .
+//         `)
+//     basic.pause(100)
+//     basic.showLeds(`
+//         . # # # .
+//         . # . . .
+//         . # # . .
+//         . # . . .
+//         . # # # .
+//         `)
+//     basic.pause(100)
+//     basic.showLeds(`
+//         . # # . .
+//         . # . # .
+//         . # # # .
+//         . # # . .
+//         . # . # .
+//         `)
+//     basic.pause(100)
+//     basic.showLeds(`
+//         . . . . .
+//         . . . . .
+//         . . . . .
+//         . . . . .
+//         . . . . .
+//         `)
+//     basic.pause(100)
+// })
+//Fun thing I did for my friend ^^^
+
+//Voting Game
+
+let myName = "Aidan"
+basic.showString(myName)
